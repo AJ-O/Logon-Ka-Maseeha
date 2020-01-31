468d0f2d4a37daf8ea81db05db191f4ac4d67afd
AJ-O <ashishleiot@gmail.com>
2020-01-31 18:28:48 +0530

    Adding favicon and resolving other issues

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-require('firebase/auth');
-require('firebase/firestore');
-require('firebase/database');
+require("firebase/auth");
+require("firebase/firestore");
+require("firebase/database");
 
-let firebase = require('firebase/app');
+let firebase = require("firebase/app");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("./public/"));
-app.use(express.static('./public/resources/'));
-app.use(express.json({limit : '1mb'}));
+app.use(express.static("./public/resources/"));
+app.use(express.json({ limit: "1mb" }));
 
 const port = 8181;
 
@@ -31,17 +31,17 @@ let firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 // firebase.analytics();
-app.post('/googleSignIn', (req, res)=>{
+app.post("/googleSignIn", (req, res) => {
   console.log("called post!");
   let token = req.body["token"];
   let userName = req.body["username"];
   let email = req.body["userEmail"];
 
   let data = {
-      password: "stud",
-      email: email,
-      token: token
-  }
+    password: "stud",
+    email: email,
+    token: token
+  };
 
   let returnObj = {
     status: "Success",
@@ -50,38 +50,38 @@ app.post('/googleSignIn', (req, res)=>{
   };
 
   let credential = firebase.auth.GoogleAuthProvider.credential(token);
-  firebase.auth().signInWithCredential(credential).catch(function(error){
-    returnObj[status] = "Failure"
-    returnObj[code] = error.code;
-    returnObj[message] = error.message;
-    console.log("Error");
-    res.send(returnObj);
-  });
-  //res.send(returnObj);
-  let users = db.ref('users');
-  users.once('value', function(snapshot){
-    if(!snapshot.child(userName).exists()){
-      console.log("User does not exist!");
-      db.ref('users/' + userName).set(
-        data
-      );
-      returnObj['userName'] = userName;
+  firebase
+    .auth()
+    .signInWithCredential(credential)
+    .catch(function(error) {
+      returnObj[status] = "Failure";
+      returnObj[code] = error.code;
+      returnObj[message] = error.message;
+      console.log("Error");
       res.send(returnObj);
-    }
-    else{
-      returnObj['userName'] = userName;
+    });
+  //res.send(returnObj);
+  let users = db.ref("users");
+  users.once("value", function(snapshot) {
+    if (!snapshot.child(userName).exists()) {
+      console.log("User does not exist!");
+      db.ref("users/" + userName).set(data);
+      returnObj["userName"] = userName;
+      res.send(returnObj);
+    } else {
+      returnObj["userName"] = userName;
       res.send(returnObj);
     }
   });
 });
 
-app.get('/:user', (req, res) => {
+app.get("/:user", (req, res) => {
   console.log("called get!");
-  console.log('Username: ' + req.params.user);
-  
+  console.log("Username: " + req.params.user);
+
   let userName = req.params.user;
-  if(userName.includes(':')){
-    userName = userName.replace(':', '');
+  if (userName.includes(":")) {
+    userName = userName.replace(":", "");
     console.log(userName);
   }
   let retObj = {
@@ -89,22 +89,22 @@ app.get('/:user', (req, res) => {
     userName: userName
   };
 
-  let userData = db.ref('users/');
-  userData.once('value', function(snapshot){
+  let userData = db.ref("users/");
+  userData.once("value", function(snapshot) {
     let userRef = snapshot.child(`/${userName}`).val();
-    retObj['email'] = userRef.email;
-    retObj['pass'] = userRef.password;
+    retObj["email"] = userRef.email;
+    retObj["pass"] = userRef.password;
     res.send(retObj);
-  }); 
-})
+  });
+});
 
-app.post('/donateItem', (req, res)=> {
+app.post("/donateItem", (req, res) => {
   let retObj = {};
   let data = req.body;
   console.log(data.userName);
-  let newItemRef = db.ref('users/' + data.userName + '/DonatedItemList').push();
+  let newItemRef = db.ref("users/" + data.userName + "/DonatedItemList").push();
   console.log(newItemRef.key); //Getting the auto generated id!
-  newItemRef.set({data}, (someParameter) => {
+  newItemRef.set({ data }, someParameter => {
     console.log("sp1: ", someParameter);
     console.log("Works");
     retObj.status = "success";
@@ -118,6 +118,6 @@ app.post('/donateItem', (req, res)=> {
   // });
 });
 
-app.listen(port, ()=>{
-    console.log(`Listening to ${port}`);
+app.listen(port, () => {
+  console.log(`Listening to ${port}`);
 });
--- public/resources/javascript/mainPage.js
+++ public/resources/javascript/mainPage.js
@@ -1,203 +1,195 @@
-//Program Flow 
-    // --   As window is loaded, items are to be displayed
-    // --  Get the username from the url
-    // --  Send the request to get Data from the server
-    // --  Listen to donate item button, onclick user will fill the form
-    // --  After user clicks donate button, data will be sent to server and saved
-    // --  When user submits, that data will be updated on the statusBoard
+//Program Flow
+// --   As window is loaded, items are to be displayed
+// --  Get the username from the url
+// --  Send the request to get Data from the server
+// --  Listen to donate item button, onclick user will fill the form
+// --  After user clicks donate button, data will be sent to server and saved
+// --  When user submits, that data will be updated on the statusBoard
 
 //To do:
-    // --  Give user prompt that data is being uploaded to the database
+// --  Give user prompt that data is being uploaded to the database
 
 window.onload = displayItems();
 
 let url = new URL(window.location);
-let username = url.searchParams.get('username');
+let username = url.searchParams.get("username");
 userSignedIn(username);
 
-function intializeBaseStuff(){
-    return new Promise((resolve, reject)=>{
+function intializeBaseStuff() {
+  return new Promise((resolve, reject) => {
     let firebaseConfig = {
-        apiKey: "AIzaSyCFdFiuXfwk3jpIYUd44XKh0Hp14l63ZJE",
-        authDomain: "logon-ka-maseeha.firebaseapp.com",
-        databaseURL: "https://logon-ka-maseeha.firebaseio.com",
-        storageBucket: "logon-ka-maseeha.appspot.com"
-        };
-        firebase.initializeApp(firebaseConfig);
-        resolve("success");
-    });
+      apiKey: "AIzaSyCFdFiuXfwk3jpIYUd44XKh0Hp14l63ZJE",
+      authDomain: "logon-ka-maseeha.firebaseapp.com",
+      databaseURL: "https://logon-ka-maseeha.firebaseio.com",
+      storageBucket: "logon-ka-maseeha.appspot.com"
+    };
+    firebase.initializeApp(firebaseConfig);
+    resolve("success");
+  });
 }
 
 const db = firebase.database();
-let latestDonatedItemId = '';
+let latestDonatedItemId = "";
 
-async function userSignedIn(username){
+async function userSignedIn(username) {
+  let response = await fetch(`/:${username}`);
+  let json = await response.json();
 
-    let response = await fetch(`/:${username}`);
-    let json = await response.json();
-    
-    if(json.status === 'Success'){
-        console.log(json);
-        let divDB = document.getElementById('dashboard');
-        if(divDB){
-            console.log("Got db");
-        }
-        else{
-            console.log("Nope, didn't get db!");
-        }
-        let userElement = document.createElement('p');
-        userElement.textContent = username;
-        divDB.appendChild(userElement);
+  if (json.status === "Success") {
+    let divDB = document.getElementById("dashboard");
+    if (divDB) {
+      console.log("Got db");
+    } else {
+      console.log("Nope, didn't get db!");
     }
-    else{
-        console.log("Nope!");
-    }
+    let userElement = document.createElement("p");
+    userElement.textContent = username;
+    divDB.appendChild(userElement);
+  } else {
+    console.log("Nope!");
+  }
 }
 
-let donateButton = document.getElementById('donateItem');
-let form = document.getElementById('donationForm');
-let donate = document.getElementById('finalDonate');
-let statusBoard = document.getElementById('statusBoard');
-let unorderedList = document.createElement('ul');
+let donateButton = document.getElementById("donateItem");
+let form = document.getElementById("donationForm");
+let donate = document.getElementById("finalDonate");
+let statusBoard = document.getElementById("statusBoard");
+let unorderedList = document.createElement("ul");
 unorderedList.style.listStyleType = "none";
 
-donateButton.addEventListener('click', showForm);
-donate.addEventListener('click', getData);
+donateButton.addEventListener("click", showForm);
+donate.addEventListener("click", getData);
 
-function showForm(){
-    form.style.display = "block";
-    donate.style.display = "block";
+function showForm() {
+  form.style.display = "block";
+  donate.style.display = "block";
 }
 
-async function getData(){
-    let blobDataResult;
-    let productType = document.getElementById('product').value;
-    let pickupAddress = document.getElementById('address').value;
-    let fileObj = document.getElementById('filename').files[0];
-    //If Donated item needs  be added asynchronously to firebase and main board then
-    //get that image url, and call uploadStatusBoard function
-    
-    let reader = new FileReader();
-    reader.onload = async function(){
-        blobDataResult = reader.result;
-        console.log(blobDataResult);
-        uploadStatusBoard(productType, pickupAddress, blobDataResult);
-        alert("Wait till item is donated!");
-        let storageRef = firebase.storage().ref();
-        let imageRef = storageRef.child(fileObj.name);
-        
-        let userImageRef = await imageRef.putString(blobDataResult, 'data_url');
-        if(userImageRef.state == "success"){
-            let imageUrl = await userImageRef.ref.getDownloadURL();
-            
-            let retObj = {
-                productType: productType,
-                pickupAddress: pickupAddress,
-                userName: username,
-                imageUrl: imageUrl
-            };
+async function getData() {
+  let blobDataResult;
+  let productType = document.getElementById("product").value;
+  let pickupAddress = document.getElementById("address").value;
+  let fileObj = document.getElementById("filename").files[0];
+  //If Donated item needs  be added asynchronously to firebase and main board then
+  //get that image url, and call uploadStatusBoard function
 
-            let option = {
-                method: "POST",
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify(retObj)
-            }
-            let response = await fetch('/donateItem', option);
-            let json = await response.json();
-            
-            if(json.status == "success"){
-                console.log(json['autoKey']);
-                alert("Data uploaded");
-                //uploadStatusBoard(productType, pickupAddress, imageUrl);
-            }
-        }
-        else{
-            console.log("Try again");
-        }
+  let reader = new FileReader();
+  reader.onload = async function() {
+    blobDataResult = reader.result;
+    console.log(blobDataResult);
+    uploadStatusBoard(productType, pickupAddress, blobDataResult);
+    alert("Wait till item is donated!");
+    let storageRef = firebase.storage().ref();
+    let imageRef = storageRef.child(fileObj.name);
+
+    let userImageRef = await imageRef.putString(blobDataResult, "data_url");
+    if (userImageRef.state == "success") {
+      let imageUrl = await userImageRef.ref.getDownloadURL();
+
+      let retObj = {
+        productType: productType,
+        pickupAddress: pickupAddress,
+        userName: username,
+        imageUrl: imageUrl
+      };
+
+      let option = {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify(retObj)
+      };
+      let response = await fetch("/donateItem", option);
+      let json = await response.json();
+
+      if (json.status == "success") {
+        console.log(json["autoKey"]);
+        alert("Data uploaded");
+        //uploadStatusBoard(productType, pickupAddress, imageUrl);
+      }
+    } else {
+      console.log("Try again");
     }
-    reader.onerror = function(){
-        console.log(reader.error);
-    }
-    //reader.readAsArrayBuffer(fileObj);
-    reader.read
-    reader.readAsDataURL(fileObj);
+  };
+  reader.onerror = function() {
+    console.log(reader.error);
+  };
+  //reader.readAsArrayBuffer(fileObj);
+  reader.readAsDataURL(fileObj);
 }
 
-async function uploadStatusBoard(p1, p2, iu){
-
-    let item = createListItem(p1, p2, iu);
-    unorderedList.prepend(item);
-    statusBoard.appendChild(unorderedList);
+async function uploadStatusBoard(p1, p2, iu) {
+  let item = createListItem(p1, p2, iu);
+  unorderedList.prepend(item);
+  statusBoard.appendChild(unorderedList);
 }
 
-function createListItem(ptype, pickupadd, imageUrl){
-    let imageEle = document.createElement('img');
-    imageEle.src = imageUrl;
-    imageEle.width = 100;
-    imageEle.height = 100;
+function createListItem(ptype, pickupadd, imageUrl) {
+  let imageEle = document.createElement("img");
+  imageEle.src = imageUrl;
+  imageEle.width = 100;
+  imageEle.height = 100;
 
-    let paraAddEle = document.createElement('p');
-    paraAddEle.textContent = pickupadd;
-    
-    let paraProEle = document.createElement('p');
-    paraProEle.textContent = ptype;
+  let paraAddEle = document.createElement("p");
+  paraAddEle.textContent = pickupadd;
 
-    let listItem = document.createElement('li');
-    // let divEle = document.createElement("div");
-    let brItem = document.createElement("br");
-    listItem.appendChild(imageEle);
-    paraProEle.style.display = "flex";
-    paraProEle.style.flexDirection = "column";
-    listItem.appendChild(paraProEle);
-    // listItem.appendChild(brItem);
-    listItem.appendChild(paraAddEle);
-    return listItem;
+  let paraProEle = document.createElement("p");
+  paraProEle.textContent = ptype;
+
+  let listItem = document.createElement("li");
+  // let divEle = document.createElement("div");
+  let brItem = document.createElement("br");
+  listItem.appendChild(imageEle);
+  paraProEle.style.display = "flex";
+  paraProEle.style.flexDirection = "column";
+  listItem.appendChild(paraProEle);
+  // listItem.appendChild(brItem);
+  listItem.appendChild(paraAddEle);
+  return listItem;
 }
 
-async function displayItems(){
-    let status = await intializeBaseStuff();
-    console.log(status);
-    if(status == "success"){
-        console.log("called!");
-        // let statusBoard = document.getElementById('statusBoard');
-        // let unorderedList = document.createElement('ul');
-        //unorderedList.style.listStyleType = "none";
-        let dbRef = db.ref('users/' + username);
-        dbRef.once('value', async function(snapshot){
-            let donatedItemList = await snapshot.child('DonatedItemList').val();//Get the values under donated item list
+async function displayItems() {
+  let status = await intializeBaseStuff();
+  console.log(status);
+  if (status == "success") {
+    console.log("called!");
+    // let statusBoard = document.getElementById('statusBoard');
+    // let unorderedList = document.createElement('ul');
+    //unorderedList.style.listStyleType = "none";
+    let dbRef = db.ref("users/" + username);
+    dbRef.once("value", async function(snapshot) {
+      let donatedItemList = await snapshot.child("DonatedItemList").val(); //Get the values under donated item list
 
-                for(item in donatedItemList){
-                    //Replace with the required field
-                    let ptype = donatedItemList[item]['data']['productType'];
-                    let imageUrl = donatedItemList[item]['data']['imageUrl'];
-                    let pickupadd = donatedItemList[item]['data']['pickupAddress'];
-                    latestDonatedItemId = item;
+      for (item in donatedItemList) {
+        //Replace with the required field
+        let ptype = donatedItemList[item]["data"]["productType"];
+        let imageUrl = donatedItemList[item]["data"]["imageUrl"];
+        let pickupadd = donatedItemList[item]["data"]["pickupAddress"];
+        latestDonatedItemId = item;
 
-                    // let imageEle = document.createElement('img');
-                    // imageEle.src = imageUrl;
-                    // imageEle.width = 100;
-                    // imageEle.height = 100;
+        // let imageEle = document.createElement('img');
+        // imageEle.src = imageUrl;
+        // imageEle.width = 100;
+        // imageEle.height = 100;
 
-                    // let paraAddEle = document.createElement('p');
-                    // paraAddEle.innerText = pickupadd;
-                    
-                    // let paraProEle = document.createElement('p');
-                    // paraProEle.innerText = ptype;
+        // let paraAddEle = document.createElement('p');
+        // paraAddEle.innerText = pickupadd;
 
-                    // let listItem = document.createElement('li');
-                    // listItem.appendChild(imageEle);
-                    // listItem.appendChild(paraProEle);
-                    // listItem.appendChild(paraAddEle);
-                    let documentItem = createListItem(ptype, pickupadd, imageUrl);
-                    //unorderedList.appendChild(listItem);
-                    unorderedList.prepend(documentItem);
-                }
-            statusBoard.appendChild(unorderedList);
-        })
-    }
-    else{
-        alert("Error loading firebase data!");
-    }
+        // let paraProEle = document.createElement('p');
+        // paraProEle.innerText = ptype;
+
+        // let listItem = document.createElement('li');
+        // listItem.appendChild(imageEle);
+        // listItem.appendChild(paraProEle);
+        // listItem.appendChild(paraAddEle);
+        let documentItem = createListItem(ptype, pickupadd, imageUrl);
+        //unorderedList.appendChild(listItem);
+        unorderedList.prepend(documentItem);
+      }
+      statusBoard.appendChild(unorderedList);
+    });
+  } else {
+    alert("Error loading firebase data!");
+  }
 }
