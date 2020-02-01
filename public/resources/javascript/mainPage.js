3ddbae80c5563f990868c601b2d2d330aa87a69d
AJ-O <ashishleiot@gmail.com>
2020-02-01 18:35:49 +0530

    Status Bar

--- public/resources/javascript/mainPage.js
+++ public/resources/javascript/mainPage.js
@@ -1,5 +1,5 @@
 //Program Flow
-// --   As window is loaded, items are to be displayed
+// --  As window is loaded, items are to be displayed
 // --  Get the username from the url
 // --  Send the request to get Data from the server
 // --  Listen to donate item button, onclick user will fill the form
@@ -7,10 +7,15 @@
 // --  When user submits, that data will be updated on the statusBoard
 
 //To do:
-// --  Give user prompt that data is being uploaded to the database
+// --  Give user prompt that data is being uploaded to the database -- Done
+// -- Have a statusbar that will display that item is being loaded! -- Done
+// -- Styling statusBar
+// -- Use maps to get user location
+// -- Send NGO's data that item is donated according to their location
+// -- Lock  the item selected by NGO
+// -- Sharing details between user and NGO
 
 window.onload = displayItems();
-
 let url = new URL(window.location);
 let username = url.searchParams.get("username");
 userSignedIn(username);
@@ -46,7 +51,9 @@ async function userSignedIn(username) {
     userElement.textContent = username;
     divDB.appendChild(userElement);
   } else {
-    console.log("Nope!");
+    console.log("Error");
+    alert("Error occured try again in some time");
+    window.location = "localhost:8181";
   }
 }
 
@@ -55,14 +62,18 @@ let form = document.getElementById("donationForm");
 let donate = document.getElementById("finalDonate");
 let statusBoard = document.getElementById("statusBoard");
 let unorderedList = document.createElement("ul");
-unorderedList.style.listStyleType = "none";
+let statusBarDiv = document.getElementById("statusBar");
+let myBar = document.getElementById("myBar");
+let uploadStatusBar = 0;
 
+unorderedList.style.listStyleType = "none";
 donateButton.addEventListener("click", showForm);
 donate.addEventListener("click", getData);
 
 function showForm() {
   form.style.display = "block";
   donate.style.display = "block";
+  myBar.style.display = "block";
 }
 
 async function getData() {
@@ -70,21 +81,28 @@ async function getData() {
   let productType = document.getElementById("product").value;
   let pickupAddress = document.getElementById("address").value;
   let fileObj = document.getElementById("filename").files[0];
-  //If Donated item needs  be added asynchronously to firebase and main board then
-  //get that image url, and call uploadStatusBoard function
 
   let reader = new FileReader();
+
   reader.onload = async function() {
     blobDataResult = reader.result;
     console.log(blobDataResult);
+    uploadStatusBar = 20;
     uploadStatusBoard(productType, pickupAddress, blobDataResult);
+    uploadStatusBarFunc();
+
     alert("Wait till item is donated!");
     let storageRef = firebase.storage().ref();
     let imageRef = storageRef.child(fileObj.name);
 
     let userImageRef = await imageRef.putString(blobDataResult, "data_url");
+    uploadStatusBar = 45;
+    uploadStatusBarFunc();
+
     if (userImageRef.state == "success") {
       let imageUrl = await userImageRef.ref.getDownloadURL();
+      uploadStatusBar = 75;
+      uploadStatusBarFunc();
 
       let retObj = {
         productType: productType,
@@ -104,12 +122,14 @@ async function getData() {
       let json = await response.json();
 
       if (json.status == "success") {
+        uploadStatusBar = 100;
+        uploadStatusBarFunc();
         console.log(json["autoKey"]);
         alert("Data uploaded");
-        //uploadStatusBoard(productType, pickupAddress, imageUrl);
       }
     } else {
       console.log("Try again");
+      alert("Error uploading image, please try again!");
     }
   };
   reader.onerror = function() {
@@ -154,9 +174,6 @@ async function displayItems() {
   console.log(status);
   if (status == "success") {
     console.log("called!");
-    // let statusBoard = document.getElementById('statusBoard');
-    // let unorderedList = document.createElement('ul');
-    //unorderedList.style.listStyleType = "none";
     let dbRef = db.ref("users/" + username);
     dbRef.once("value", async function(snapshot) {
       let donatedItemList = await snapshot.child("DonatedItemList").val(); //Get the values under donated item list
@@ -167,24 +184,7 @@ async function displayItems() {
         let imageUrl = donatedItemList[item]["data"]["imageUrl"];
         let pickupadd = donatedItemList[item]["data"]["pickupAddress"];
         latestDonatedItemId = item;
-
-        // let imageEle = document.createElement('img');
-        // imageEle.src = imageUrl;
-        // imageEle.width = 100;
-        // imageEle.height = 100;
-
-        // let paraAddEle = document.createElement('p');
-        // paraAddEle.innerText = pickupadd;
-
-        // let paraProEle = document.createElement('p');
-        // paraProEle.innerText = ptype;
-
-        // let listItem = document.createElement('li');
-        // listItem.appendChild(imageEle);
-        // listItem.appendChild(paraProEle);
-        // listItem.appendChild(paraAddEle);
         let documentItem = createListItem(ptype, pickupadd, imageUrl);
-        //unorderedList.appendChild(listItem);
         unorderedList.prepend(documentItem);
       }
       statusBoard.appendChild(unorderedList);
@@ -192,4 +192,9 @@ async function displayItems() {
   } else {
     alert("Error loading firebase data!");
   }
+}
+
+function uploadStatusBarFunc() {
+  myBar.style.width = uploadStatusBar + "%";
+  myBar.textContent = uploadStatusBar + "%";
 }
